0d847976466c09cc91c0536430cc6773898e2210
matheusssilva <matheus.saraiva@gmail.com>
2022-06-28 12:41:58 -0300

    Criação função de validar CNPJ

--- tests/validations_test.go
+++ tests/validations_test.go
@@ -3,7 +3,7 @@ package tests
 import (
 	"testing"
 
-	generalgo "github.com/StackSistemas/generalgo"
+	"github.com/StackSistemas/generalgo"
 )
 
 func TestStringMatch(t *testing.T) {
@@ -32,6 +32,41 @@ func TestStringMatch(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := generalgo.StringMatch(tt.args.value, tt.args.pattern); got != tt.want {
 				t.Errorf("StringMatch() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestCnpjIsValid(t *testing.T) {
+	type args struct {
+		docnum string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		// TODO: Add test cases.
+		{
+			name: "CnpjValid",
+			args: args{"59541264000103"},
+			want: true,
+		},
+		{
+			name: "CnpjWithInvalidDigit",
+			args: args{"59541264000144"},
+			want: false,
+		},
+		{
+			name: "CnpjWithInvalidLen",
+			args: args{"595412640001"},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := generalgo.CnpjIsValid(tt.args.docnum); got != tt.want {
+				t.Errorf("CnpjIsValid() = %v, want %v", got, tt.want)
 			}
 		})
 	}
--- validations.go
+++ validations.go
@@ -2,6 +2,7 @@ package generalgo
 
 import (
 	"regexp"
+	"strconv"
 )
 
 const (
@@ -14,6 +15,12 @@ const (
 
 	//MinNamesDescriptionsLength é o tamanho mínimo para nomes de pessoas.
 	MinNamesDescriptionsLength uint8 = 3
+
+	//CpfRegex é um padrão regex para formato de número de cpf.
+	CpfRegexNoMask = `^[0-9]{11}$`
+
+	//CnpjRegex é um padrão regex para formato de número de cpf.
+	CnpjRegexNoMask = `^[0-9]{14}$`
 )
 
 //StringMatch valida uma string com base em um padrão regex. Retorna true se corresponde e false se não.
@@ -21,4 +28,52 @@ func StringMatch(value, pattern string) bool {
 	var rgx *regexp.Regexp = regexp.MustCompile(pattern)
 
 	return rgx.MatchString(value)
+}
+
+//CpfIsValid valida se um número de cpf é valido.
+func CpfIsValid(docnum string) bool {
+
+	return false
+}
+
+// CnpjIsValid valida se um número de CNPJ é válido.
+func CnpjIsValid(docnum string) bool {
+
+	if !StringMatch(docnum, CnpjRegexNoMask) {
+		return false
+	}
+
+	var cnpj_valid string = docnum[:12]
+
+	var base, digit uint8
+	var sum uint16
+
+CALCDIGIT:
+
+	base, digit, sum = 2, 0, 0
+
+	for i, j := 0, len(cnpj_valid)-1; j >= 0; i, j = i+1, j-1 {
+
+		if base > 9 {
+			base = 2
+		}
+
+		sum += uint16(uint8(cnpj_valid[j]-'0') * base)
+
+		base++
+	}
+
+	digit = uint8(11 - (sum % 11))
+
+	if digit >= 10 {
+		cnpj_valid += "0"
+	} else {
+		cnpj_valid += strconv.Itoa(int(digit))
+	}
+
+	if len(cnpj_valid) < 14 {
+		goto CALCDIGIT
+	}
+
+	return cnpj_valid == docnum
 }
